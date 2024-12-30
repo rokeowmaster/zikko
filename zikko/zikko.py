7d9ae6a4981341bec8b5d8592cbc4892de180aa6
unknown <rokeowmaster@gmail.com>
2024-12-30 07:50:23 +0300

    boilerplate

---
+++ zikko/zikko.py
@@ -0,0 +1,209 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+    ...
+
+
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.vstack(
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2.25em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Zikko", size="7", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.hstack(
+                    rx.link("Home", href="/#"),
+                    rx.link("About", href="/#"),
+                    rx.link("Join", href="/signup"),
+                    rx.link("Contact", href="/#"),
+                    rx.link("Jobs", href="/jobs"),
+                    justify="end",
+                    spacing="5",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.menu.root(
+                    rx.menu.trigger(
+                        rx.icon("menu", size=30)
+                    ),
+                    rx.menu.content(
+                        rx.menu.item("Home"),
+                        rx.menu.item("About"),
+                        rx.menu.item("Pricing"),
+                        rx.menu.item("Contact"),
+                    ),
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.image(src="/jobs.jpg", width="100px", height="auto"),
+
+
+    )
+
+
+def jobs():
+    return rx.text("Available jobs")
+
+# @rx.page(route="/signup", title="Register")
+def signup():
+    return rx.center(
+        rx.card(
+        rx.vstack(
+            rx.flex(
+                rx.image(
+                    src="/logo.jpg",
+                    width="2.5em",
+                    height="auto",
+                    border_radius="25%",
+                ),
+                rx.heading(
+                    "Create an account",
+                    size="6",
+                    as_="h2",
+                    width="100%",
+                ),
+                rx.hstack(
+                    rx.text(
+                        "Already registered?",
+                        size="3",
+                        text_align="left",
+                    ),
+                    rx.link("Sign in", href="#", size="3"),
+                    spacing="2",
+                    opacity="0.8",
+                    width="100%",
+                ),
+                justify="start",
+                direction="column",
+                spacing="4",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Email address",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("user")),
+                    placeholder="user@reflex.dev",
+                    type="email",
+                    size="3",
+                    width="100%",
+                ),
+                justify="start",
+                spacing="2",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Password",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("lock")),
+                    placeholder="Enter your password",
+                    type="password",
+                    size="3",
+                    width="100%",
+                ),
+                justify="start",
+                spacing="2",
+                width="100%",
+            ),
+            rx.box(
+                rx.checkbox(
+                    "Agree to Terms and Conditions",
+                    default_checked=True,
+                    spacing="2",
+                ),
+                width="100%",
+            ),
+            rx.button("Register", size="3", width="100%"),
+            rx.hstack(
+                rx.divider(margin="0"),
+                rx.text(
+                    "Or continue with",
+                    white_space="nowrap",
+                    weight="medium",
+                ),
+                rx.divider(margin="0"),
+                align="center",
+                width="100%",
+            ),
+            rx.center(
+                rx.icon_button(
+                    rx.icon(tag="github"),
+                    variant="soft",
+                    size="3",
+                ),
+                rx.icon_button(
+                    rx.icon(tag="facebook"),
+                    variant="soft",
+                    size="3",
+                ),
+                rx.icon_button(
+                    rx.icon(tag="twitter"),
+                    variant="soft",
+                    size="3",
+                ),
+                spacing="4",
+                direction="row",
+                width="100%",
+            ),
+            spacing="6",
+            width="100%",
+        ),
+        size="4",
+        max_width="28em",
+        width="100%",
+        text_align="center"
+    )
+    )
+
+
+app = rx.App()
+app.add_page(index)
+app.add_page(signup)
+app.add_page(jobs)
